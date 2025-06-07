d688e2c308019e316935d8022d7a15ba310d2336
PolyFacto <164651706+PolyFacto@users.noreply.github.com>
2025-06-07 15:06:05 +0200

    Initial commit

---
+++ App/Source/TestCommit.cpp
@@ -0,0 +1,5 @@
+#include "TestCommit.h"
+
+namespace Core {
+    
+} // Core
---
+++ App/Source/TestCommit.h
@@ -0,0 +1,10 @@
+#pragma once
+
+namespace Core {
+    class TestCommit 
+    {
+    public:
+        TestCommit();
+        ~TestCommit();
+    };
+} // Core
